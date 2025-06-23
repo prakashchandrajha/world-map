789406a8fb5443d0282b57f081a51f6232aad4ae
Your Name <your.email@example.com>
2025-06-23 17:23:51 +0530

    add header and carousel

--- src/app/components/heritage-site/heritage-site.component.ts
+++ src/app/components/heritage-site/heritage-site.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from "../../shared/carousel/carousel.component";
 
 @Component({
   selector: 'app-heritage-site',
-  imports: [],
+  imports: [CarouselComponent],
   templateUrl: './heritage-site.component.html',
   styleUrl: './heritage-site.component.css'
 })
