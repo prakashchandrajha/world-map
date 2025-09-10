2b091d4025aff34fa017c4d58e70fee844d274c1
Your Name <your.email@example.com>
2025-09-10 16:14:41 +0530

    change landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { WorldMapComponent } from './components/world-map/world-map.component';
 import { HeritageSiteComponent } from './components/heritage-site/heritage-site.component';
 import { MemberComponent } from './components/member/member.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 export const routes: Routes = [
     {
-        path: '', component: WorldMapComponent
+        path: '', component: LandingPageComponent
     },
     { path: 'heritage-site/:id', component: HeritageSiteComponent },
     { path: 'heritage-site', component: HeritageSiteComponent },
---
+++ src/assets/images/components/landing-page/landing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../../app/shared/header/header.component";
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [HeaderComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent {
+
+}
